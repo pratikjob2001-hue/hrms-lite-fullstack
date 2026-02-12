f4c766e8a0bc39537ff589bd3fec4f132e217c36
SMITA324/order-food <smitashinde2110@gmail.com>
2026-02-12 14:20:32 +0530

    Auto create superuser

--- backend/hrms_backend/manage.py
+++ backend/hrms_backend/manage.py
@@ -7,14 +7,32 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrms_backend.settings')
+
     try:
         from django.core.management import execute_from_command_line
+        from django.contrib.auth import get_user_model
+        import django
+
+        # Setup Django
+        django.setup()
+
+        # Create superuser automatically (ONLY if not exists)
+        User = get_user_model()
+
+        username = "admin"
+        email = "admin@gmail.com"
+        password = "admin123"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
